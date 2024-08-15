f6eb7a953cc6938164242923cf815c429c5ede8f
YulianSan <knoob0733@gmail.com>
2024-08-14 22:57:26 -0300

    improving performance

--- src/zigzag_conversion.rs
+++ src/zigzag_conversion.rs
@@ -2,32 +2,28 @@ pub struct Solution {}
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        let letter_by_zig = (num_rows * 2 - 2).max(num_rows);
-        let mut zigzag_string = String::new();
-        let chars = s.chars();
-        let len = s.len();
-        let cols = len.div_ceil(letter_by_zig as usize) as i32;
+        let mut direction: i32 = 1;
+        let mut rows = vec![String::new(); num_rows as usize];
+        let mut c_row: i32 = 0;
 
-        for row in 0..num_rows {
-            for col in 0..cols {
-                let current_letter = (col * letter_by_zig) + row;
-                let current_letter_back = ((col + 1) * letter_by_zig) - row;
+        if num_rows == 1 {
+            return s;
+        }
 
-                if (current_letter as usize) < s.len() {
-                    if let Some(c) = chars.clone().skip((current_letter) as usize).next() {
-                        zigzag_string.push(c);
-                    }
-                }
+        for c in s.chars() {
+            rows[c_row as usize].push(c);
 
-                if current_letter_back != current_letter && row != 0 && num_rows - 1 != row {
-                    if let Some(c) = chars.clone().skip((current_letter_back) as usize).next() {
-                        zigzag_string.push(c)
-                    }
-                }
+            c_row += direction;
+
+            if c_row == 0 || c_row == num_rows - 1 {
+                direction *= -1;
             }
         }
 
-        zigzag_string
+        rows.iter().fold(String::new(), |mut acc, row| {
+            acc.push_str(row);
+            acc
+        })
     }
 }
 
@@ -52,5 +48,6 @@ mod test {
         check_zigzag!("PAYPALISHIRING" with 3 expect "PAHNAPLSIIGYIR");
         check_zigzag!("A" with 1 expect "A");
         check_zigzag!("ABCD" with 3 expect "ABDC");
+        check_zigzag!("AB" with 1 expect "AB");
     }
 }
